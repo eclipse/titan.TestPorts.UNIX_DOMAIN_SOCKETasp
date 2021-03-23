a96ddafc47d7ee1d8895199b1a67b1c792a84bcd
Vadim Yanitskiy <axilirator@gmail.com>
2021-03-23 07:08:43 +0100

    On socket error/close: send UD_connect_result with ERROR
    Signed-off-by: Vadim Yanitskiy <axilirator@gmail.com>

--- src/UD_PT.cc
+++ src/UD_PT.cc
@@ -185,6 +185,13 @@ void UD__PT_PROVIDER::Handle_Fd_Event_Readable(int fd)
 						Handler_Remove_Fd_Read(close_fd);
 						close(close_fd);
 
+						//notify the port user about that
+						UD__Types::UD__connect__result cr;
+						cr.id() = a;
+						cr.result()().result__code() = UD__Types::UD__Result__code::ERROR_;
+						cr.result()().err() = "Connection closed by remote peer";
+						incoming_message(cr);
+
 						//unlink(conn_list[a].remote_Addr.sun_path);
 					}
 					else{
